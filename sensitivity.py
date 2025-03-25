b2f819b80bbb178a21c604bbb89cc402a0631549
Jahanvi-Phd <jahanvi222@gmail.com>
2025-03-25 22:20:35 +0530

    updated

--- sensitivity.py
+++ sensitivity.py
@@ -9,7 +9,7 @@ from collections import Counter
 
 #Current timestamp
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_folder = "test_output"
+output_folder = f"test_output/output_{timestamp}"
 os.makedirs(output_folder, exist_ok=True)
 # Set random seed for reproducibility
 np.random.seed(42)
